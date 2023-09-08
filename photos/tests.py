9a5be1d2ed0b82674058694e96e80b7701c795b0
moawezz <moawez.de@gmail.com>
2023-09-08 19:03:22 +0330

    Add tests for post list view and detail view

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class PhotoAppTest(TestCase):
+
+    def test_photo_list_view_by_url(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_photo_list_view_by_url_name(self):
+        response = self.client.get(reverse('photo_list'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_photo_detail_view_by_url(self):
+        response = self.client.get('/2/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_photo_detail_view_by_url_name(self):
+        response = self.client.get(reverse('photo_detail', args=['3']))
+        self.assertEqual(response.status_code, 200)
