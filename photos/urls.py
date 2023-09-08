c7be313f333f30eebcce302fee1a08f871bfcd33
moawezz <moawez.de@gmail.com>
2023-09-08 19:48:03 +0330

    Add style to photo list and photo detial template

--- photos/urls.py
+++ photos/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import photo_list_view, photo_detail_view
+from .views import photo_list_view, photo_detail_view, add_photo_view
 
 urlpatterns = [
     path('', photo_list_view, name='photo_list'),
-    path('photos/<int:pk>/', photo_detail_view, name='photo_detail'),
-    path('add/', '', name='add_photo'),
+    path('<int:pk>/', photo_detail_view, name='photo_detail'),
+    path('add/', add_photo_view, name='add_photo'),
 ]
--- photos/views.py
+++ photos/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 
 def photo_list_view(request):
-    return HttpResponse('List')
+    return render(request, 'photos/photo_list.html')
 
 def photo_detail_view(request, pk):
-    return HttpResponse('Detail')
+    return render(request, 'photos/photo_detail.html')
 
 def add_photo_view(request):
-    return HttpResponse('Add')
+    return render(request, 'photos/photo_create.html')
