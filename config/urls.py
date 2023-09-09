3e4d1bff3a6b1761527c70f6c42887b303e7935c
moawezz <moawez.de@gmail.com>
2023-09-09 18:59:47 +0330

    Add account templates and urls

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', include('photos.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
