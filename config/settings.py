b80fda7c033b5b2de6c957ca39fd81c771b79954
moawezz <moawez.de@gmail.com>
2023-09-09 18:41:08 +0330

    Add accounts app for custom-user

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # local apps
+    'accounts.apps.AccountsConfig',
     'photos.apps.PhotosConfig',
 ]
 
@@ -130,3 +132,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# accounts setting
+AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = ''
+LOGOUT_REDIRECT_URL = ''
