d27e9beaca633d346be62611773ab24276d8ba3c
moawezz <moawez.de@gmail.com>
2023-09-09 20:18:28 +0330

    Create photo and category models and registered them

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Photo, Category
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    model = Photo
+    list_display = ['title', 'category',]
+
+admin.site.register(Category)
--- photos/models.py
+++ photos/models.py
@@ -2,10 +2,22 @@ from django.db import models
 
 from django.urls import reverse
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
 class Photo(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     image = models.ImageField(upload_to='images/')
+    category = models.ForeignKey(to=Category, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
