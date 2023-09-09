b92f540f61e5eed5fccf680c857454a195544186
moawezz <moawez.de@gmail.com>
2023-09-09 19:12:04 +0330

    Add absolute urls to photo model

--- config/settings.py
+++ config/settings.py
@@ -60,6 +60,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Add the account middleware:
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
--- photos/models.py
+++ photos/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+from django.urls import reverse
 
 class Photo(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("photo_detail", kwargs={"pk": self.pk})
+
+    
